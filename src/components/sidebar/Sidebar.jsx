a7ad4a6f840213452bb5d06094343d3328d7807a
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-09-25 22:51:34 +1000

    navbar design

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -80,7 +80,6 @@ const Sidebar = () => {
       <div className="bottom">
         <div className="colorOption"></div>
         <div className="colorOption"></div>
-        <div className="colorOption"></div>
       </div>
     </div>
   );
