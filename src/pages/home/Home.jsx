1cad2c1f432224e737f81d314036faf6f4896e45
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-09-29 14:20:24 +1000

    widgets components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        home container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+
+          <Widget type="balance" />
+        </div>
+        <div className="charts"></div>
       </div>
     </div>
   );
